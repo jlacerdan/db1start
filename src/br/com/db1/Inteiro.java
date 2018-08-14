9e6131381f86b0ab7dbc96f6665d16e2e8935787
João <joao.lacerda0@gmail.com>
2018-08-14 08:37:04 -0300

    Aula 03 DB1START

---
+++ src/br/com/db1/Inteiro.java
@@ -0,0 +1,32 @@
+package br.com.db1;
+
+public class Inteiro {
+
+	public Integer Soma(Integer valor1, Integer valor2) {
+		return valor1 + valor2;
+	}
+	
+	public Integer Subtracao(Integer valor1, Integer valor2) {
+		return valor1 - valor2;
+	}
+	
+	public Integer Multiplicacao(Integer valor1, Integer valor2) {
+		return valor1 * valor2;
+	}
+	
+	public Integer Divisao(Integer valor1, Integer valor2) {
+		if (valor2 == 0) {
+			throw new ArithmeticException();
+		}
+		return valor1 / valor2;
+	}
+	
+	public Boolean ehPar(Integer parametro) {
+		return parametro % 2 == 0;		
+	}
+	
+	public Boolean ehMaior(Integer valor1, Integer valor2){
+		return valor1 > valor2;
+	}
+
+}
